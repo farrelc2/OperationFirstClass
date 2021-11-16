ecae00de44b87a39edb8c4b08fd94bac54880d1d
BMButcher <92819229+BMButcher@users.noreply.github.com>
2021-11-16 11:14:53 -0700

    Add files via upload
    Score card header file

---
+++ ScoreCard.h
@@ -0,0 +1,33 @@
+class ScoreCard{
+    private:
+    int aces = 0;
+    int twos = 0;
+    int threes = 0;
+    int fours = 0;
+    int fives = 0;
+    int sixes = 0;
+    int upperSectionTotal = 0;
+    int upperBonus = 0;
+    int upperSectionTotalBonus = 0;
+    
+    int threeOfAKind = 0;
+    int fourOfAKind = 0;
+    int fullHouse = 0;
+    int smallStraight = 0;
+    int largeStraight = 0;
+    int yahtzee = 0;
+    int chance = 0;
+    int lowerBonus = 0;
+    int lowerSectionTotal = 0;
+    
+    public:
+    ScoreCard(Yahtzee& currentRoll){ R = currentRoll; }
+    int selectScore;
+    Yahtzee R;
+    void updateRound(Yahtzee& y){
+        R = y;
+    }
+    void upperScoreCard();
+    void lowerScoreCard();
+    void displayScoreCard();
+};
